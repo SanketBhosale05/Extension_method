0767238994e456b75b6000cf162f73ef58eba56c
91837 <91837@SAHIL>
2023-07-14 11:52:40 +0530

    Add project files.

---
+++ Mathametical_opertion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_method
+{
+  public class Calculation  
+    {
+
+    }
+    public static class ExtensionClass
+    { 
+        public static  int Sqaure (this Calculation c,int x)
+        {
+            return x*x;
+
+
+        }
+
+    }
+    public static class ExtensionClass1
+    {
+        public static int multiply ( this Calculation c,int x,int y)
+        {
+            return x*y ;
+
+        }
+    }
+    public static class ExtensionClass2
+    {
+        public static int Divide( this Calculation calculation,int x,int y)
+        {
+            return x/y ;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c1 = new Calculation();
+            int result1 = c1.Sqaure(2);
+            Console.WriteLine(result1);
+
+            int result2 = c1.multiply(2, 3);
+            Console.WriteLine(result2);
+
+             double res = c1.Divide(2, 3);
+            Console.WriteLine(res);
+        }
+    }
+}
